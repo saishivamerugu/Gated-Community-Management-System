f00f5f64b22d65b981d87ad1d4355bbc82877346
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-01-31 11:00:38 +0530

    Refactor ResidentDao class for improved readability

--- GatedCommunityProject/src/main/java/com/project/dao/ResidentDao.java
+++ GatedCommunityProject/src/main/java/com/project/dao/ResidentDao.java
@@ -10,10 +10,8 @@ import util.HibernateUtil;
 public class ResidentDao {
 
     public void saveResident(Resident resident) {
-
         Session session = null;
         Transaction tx = null;
-
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             tx = session.beginTransaction();
@@ -32,28 +30,24 @@ public class ResidentDao {
 
         try {
             session = HibernateUtil.getSessionFactory().openSession();
-
             String hql = "FROM Resident r WHERE r.username = :username AND r.password = :password";
             Query<Resident> query = session.createQuery(hql, Resident.class);
             query.setParameter("username", username);
             query.setParameter("password", password);
-
             resident = query.uniqueResult();
 
         } catch (Exception e) {
             e.printStackTrace();
         } 
-        
         return resident;
     }
     
     public Resident getResidentById(int resId) {
-
         Session session = HibernateUtil.getSessionFactory().openSession();
         Resident resident = session.get(Resident.class, resId);
         session.close();
-
         return resident;
     }
 
 }
+
